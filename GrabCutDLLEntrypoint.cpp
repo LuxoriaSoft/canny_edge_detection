c939600dce6a8cb4bb7049ac05169be1e16fc0fa
QuentinCraft <quentin.erdinger@epitech.eu>
2025-03-17 15:28:48 +0000

    refactor: Update GrabCutDLLEntrypoint.cpp to include GrabCutAlgorithm.hpp and clarify implementation details

--- GrabCutDLLEntrypoint.cpp
+++ GrabCutDLLEntrypoint.cpp
@@ -1,1 +1,4 @@
-#include "GrabCut.hpp"
+#include "GrabCutAlgorithm.hpp"
+
+// Nothing further is needed here. The GrabCutAlgorithm.hpp header file contains all the necessary code.
+// The implementation of the GrabCutWrapper class is in the GrabCutAlgorithm.hpp (header) file.
